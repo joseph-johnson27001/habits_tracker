12dbd5dc7fb87f3111576bf47265fe36625dcc95
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-09-11 17:02:25 +0100

    Profile header first layout

---
+++ src/components/ProfileHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="profile-header">
+    <div class="user-info">
+      <h2>Joe Johnson</h2>
+      <p>Web Developer</p>
+    </div>
+    <div class="profile-picture">
+      <img src="@/assets/images/joe-taj.jpg" alt="Profile Picture" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileHeader",
+};
+</script>
+
+<style scoped>
+.profile-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  background-color: #f0f0f0;
+}
+
+.user-info {
+  font-size: 18px;
+  margin-left: 20px;
+}
+
+.profile-picture img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+</style>
--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,14 +1,35 @@
 <template>
-  <div>
-    <h1>Hello, World</h1>
-    <!-- Add more content for your profile page here -->
+  <div class="profile-page">
+    <ProfileHeader />
+    <!-- <AchievementsSection />
+    <BadgesSection />
+    <StatisticsSection />
+    <HabitStreaksSection />
+    <CompletedHabitsSection />
+    <TrackerSection /> -->
   </div>
 </template>
 
 <script>
+import ProfileHeader from "@/components/ProfileHeader.vue";
+// import AchievementsSection from "./AchievementsSection.vue";
+// import BadgesSection from "./BadgesSection.vue";
+// import StatisticsSection from "./StatisticsSection.vue";
+// import HabitStreaksSection from "./HabitStreaksSection.vue";
+// import CompletedHabitsSection from "./CompletedHabitsSection.vue";
+// import TrackerSection from "./TrackerSection.vue";
+
 export default {
   name: "ProfilePage",
-  // Add component-specific logic here
+  components: {
+    ProfileHeader,
+    // AchievementsSection,
+    // BadgesSection,
+    // StatisticsSection,
+    // HabitStreaksSection,
+    // CompletedHabitsSection,
+    // TrackerSection,
+  },
 };
 </script>
 
