d939d830d3d732655e3809d555fbeb5dd135b9d2
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-09-12 10:24:58 +0100

    news feed components layout

---
+++ src/components/NewsFeed/SocialStatistics.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>HELLO</h1>
+</template>
+
+<script>
+export default {
+  name: "SocialStats",
+  data() {},
+};
+</script>
--- src/views/NewsFeedPage.vue
+++ src/views/NewsFeedPage.vue
@@ -1,14 +1,45 @@
 <template>
-  <div class="news"></div>
+  <div class="news">
+    <div class="news-feed">
+      <NewsFeed />
+    </div>
+    <div class="social-stats">
+      <SocialStats />
+    </div>
+  </div>
 </template>
 
 <script>
+import NewsFeed from "@/components/NewsFeed/NewsFeed.vue";
+import SocialStats from "@/components/NewsFeed/SocialStatistics.vue";
+
 export default {
   name: "NewsPage",
-  components: {},
+  components: {
+    NewsFeed,
+    SocialStats,
+  },
+  data() {},
 };
 </script>
 
 <style scoped>
-/* Add component-specific styles here */
+.news {
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  height: 100vh;
+}
+
+.news-feed {
+  padding: 10px;
+  border-right: 1px solid #ccc;
+  height: 100%;
+  overflow-y: auto;
+}
+
+.social-stats {
+  padding: 10px;
+  height: 100%;
+  overflow-y: auto;
+}
 </style>
