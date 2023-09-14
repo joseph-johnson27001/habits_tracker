8d6b5be7ae541995c9e8d389982cf39c79a8e920
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-09-14 13:09:59 +0100

    Statistics section

--- src/components/ProfilePage/StatisticsSection.vue
+++ src/components/ProfilePage/StatisticsSection.vue
@@ -1,16 +1,68 @@
 <template>
   <div class="statistics-section">
-    <h1>STATISTICS SECTION</h1>
+    <h1>STATISTICS</h1>
+    <div class="statistics-summary">
+      <div class="stat-item">
+        <div class="stat-label">Total Completed Habits</div>
+        <div class="stat-value">{{ totalCompletedHabits }}</div>
+      </div>
+      <div class="stat-item">
+        <div class="stat-label">Longest Habit Streak</div>
+        <div class="stat-value">{{ longestHabitStreak }} days</div>
+      </div>
+      <div class="stat-item">
+        <div class="stat-label">Habits Completed Today</div>
+        <div class="stat-value">{{ habitsCompletedToday }}</div>
+      </div>
+      <div class="stat-item">
+        <div class="stat-label">Habits Completed This Week</div>
+        <div class="stat-value">{{ habitsCompletedThisWeek }}</div>
+      </div>
+      <div class="stat-item">
+        <div class="stat-label">Habits Completed This Month</div>
+        <div class="stat-value">{{ habitsCompletedThisMonth }}</div>
+      </div>
+      <div class="stat-item">
+        <div class="stat-label">Habits Completed This Year</div>
+        <div class="stat-value">{{ habitsCompletedThisYear }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "StatisticsSection",
-  // Add your component logic here
+  data() {
+    return {
+      totalCompletedHabits: 245,
+      longestHabitStreak: 42,
+      habitsCompletedToday: 5,
+      habitsCompletedThisWeek: 32,
+      habitsCompletedThisMonth: 78,
+      habitsCompletedThisYear: 210,
+    };
+  },
 };
 </script>
 
 <style scoped>
-/* Add component-specific styles here */
+.stat-item {
+  margin-bottom: 20px;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between; /* Align values to both ends */
+  border-bottom: 1px solid #ccc;
+  background-color: white;
+}
+
+.stat-label {
+  font-weight: bold;
+  font-size: 16px;
+}
+
+.stat-value {
+  font-size: 18px;
+  color: #333;
+}
 </style>
