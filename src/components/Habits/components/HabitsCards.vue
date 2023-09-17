493cabf4b07c412abc4c35e71513e88eb560bd77
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-09-17 11:02:36 +0100

    Habit Cards and containers

---
+++ src/components/Habits/components/HabitsCards.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HabitsCards",
+};
+</script>
+
+<style scoped></style>
--- src/views/HabitsPage.vue
+++ src/views/HabitsPage.vue
@@ -1,3 +1,31 @@
 <template>
-  <div class="card">Habits</div>
+  <div class="habits-page">
+    <div class="card">
+      <div class="heading-container">
+        <h1>Meditating</h1>
+      </div>
+
+      <HabitsCards />
+    </div>
+  </div>
 </template>
+
+<script>
+import HabitsCards from "@/components/Habits/components/HabitsCards.vue";
+
+export default {
+  components: {
+    HabitsCards,
+  },
+};
+</script>
+
+<style>
+.habits-page {
+  padding: 10px;
+}
+.heading-container {
+  border-bottom: 1px solid #ccc;
+  padding: 5px;
+}
+</style>
