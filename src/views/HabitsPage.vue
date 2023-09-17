c3338d6fd61e2560f96573d6d6919ae37d56fd98
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-09-17 11:40:11 +0100

    Carousel animation working correctly

--- src/views/HabitsPage.vue
+++ src/views/HabitsPage.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="habits-page">
-    <!-- Display the current habit card -->
-    <div class="card">
-      <div class="heading-container">
-        <h1>{{ habits[currentIndex].name }}</h1>
+    <!-- Display the current habit card with slide animation -->
+    <transition :name="transitionName" mode="out-in">
+      <div :key="currentIndex" class="card">
+        <div class="heading-container">
+          <h1>{{ habits[currentIndex].name }}</h1>
+        </div>
+        <div class="habit-card-content">
+          <p>{{ habits[currentIndex].value }}</p>
+          <HabitCard :habit="habits[currentIndex]" />
+        </div>
       </div>
-      <p>{{ habits[currentIndex].value }}</p>
-      <HabitCard :habit="habits[currentIndex]" />
-    </div>
+    </transition>
 
     <!-- Add navigation buttons to move between cards -->
     <div class="carousel-navigation">
@@ -34,16 +38,28 @@ export default {
         { name: "Reading", value: "Reading Graph Goes Here" },
       ],
       currentIndex: 0, // Initialize with the first card
+      direction: "next", // Initialize animation direction
     };
+  },
+  computed: {
+    transitionName() {
+      return this.direction === "prev" ? "slide-fade-prev" : "slide-fade-next";
+    },
   },
   methods: {
     nextCard() {
+      // Set animation direction to "next"
+      this.direction = "next";
+
       // Move to the next card if not at the end
       if (this.currentIndex < this.habits.length - 1) {
         this.currentIndex++;
       }
     },
     prevCard() {
+      // Set animation direction to "prev"
+      this.direction = "prev";
+
       // Move to the previous card if not at the beginning
       if (this.currentIndex > 0) {
         this.currentIndex--;
@@ -65,5 +81,27 @@ export default {
   display: flex;
   justify-content: space-between;
   margin-top: 10px;
+}
+.card {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  transition: transform 0.86s, opacity 0.5s;
+}
+.slide-fade-next-enter-active,
+.slide-fade-prev-leave-active,
+.slide-fade-prev-enter-active,
+.slide-fade-next-leave-active {
+  transition: transform 0.5s, opacity 0.5s;
+}
+.slide-fade-next-enter,
+.slide-fade-prev-leave-to {
+  transform: translateX(100%);
+  opacity: 0;
+}
+.slide-fade-prev-enter,
+.slide-fade-next-leave-to {
+  transform: translateX(-100%);
+  opacity: 0;
 }
 </style>
