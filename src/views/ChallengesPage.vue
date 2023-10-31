83e6025e80e52e2f4f3b08f8edeb3fb25f77096e
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-10-31 11:13:58 +0700

    8 dummy challenges

--- src/views/ChallengesPage.vue
+++ src/views/ChallengesPage.vue
@@ -59,7 +59,85 @@ export default {
             { name: "Mindfulness Pro Title", icon: "🌟" },
           ],
         },
-        // Add more dummy challenges as needed
+        {
+          id: 3,
+          title: "Healthy Eating Challenge",
+          description:
+            "Join our healthy eating challenge to develop good dietary habits and improve your well-being!",
+          startDate: "2023-11-15",
+          endDate: "2023-12-15",
+          participants: 90,
+          awards: [
+            { name: "Nutrition Expert Badge", icon: "🥑" },
+            { name: "Healthy Eater Title", icon: "🍏" },
+          ],
+        },
+        {
+          id: 4,
+          title: "Yoga Journey Challenge",
+          description:
+            "Embark on a transformative yoga journey to enhance your flexibility and inner strength.",
+          startDate: "2023-12-05",
+          endDate: "2024-01-05",
+          participants: 75,
+          awards: [
+            { name: "Yoga Master Badge", icon: "🧘" },
+            { name: "Mind-Body Harmony Title", icon: "🕉️" },
+          ],
+        },
+        {
+          id: 5,
+          title: "Reading Challenge",
+          description:
+            "Immerse yourself in the world of literature with our exciting reading challenge!",
+          startDate: "2023-11-10",
+          endDate: "2023-12-10",
+          participants: 110,
+          awards: [
+            { name: "Bookworm Badge", icon: "📚" },
+            { name: "Literary Guru Title", icon: "📖" },
+          ],
+        },
+        {
+          id: 6,
+          title: "Mindful Walking Challenge",
+          description:
+            "Take mindful steps towards better health and mindfulness in our walking challenge!",
+          startDate: "2023-11-20",
+          endDate: "2023-12-20",
+          participants: 95,
+          awards: [
+            { name: "Walking Enthusiast Badge", icon: "🚶" },
+            { name: "Mindful Walker Title", icon: "🌳" },
+          ],
+        },
+        {
+          id: 7,
+          title: "Daily Water Intake Challenge",
+          description:
+            "Stay hydrated and improve your health with our daily water intake challenge!",
+          startDate: "2023-11-25",
+          endDate: "2023-12-25",
+          participants: 100,
+          awards: [
+            { name: "Hydration Hero Badge", icon: "💧" },
+            { name: "Water Champion Title", icon: "🚰" },
+          ],
+        },
+        {
+          id: 8,
+          title: "Creativity Boost Challenge",
+          description:
+            "Unleash your creativity with our engaging and inspiring creativity boost challenge!",
+          startDate: "2023-12-15",
+          endDate: "2024-01-15",
+          participants: 80,
+          awards: [
+            { name: "Creative Thinker Badge", icon: "🎨" },
+            { name: "Innovation Maestro Title", icon: "🖌️" },
+          ],
+        },
+        // Add more challenges as needed
       ],
     };
   },
