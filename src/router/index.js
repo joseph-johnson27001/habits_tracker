2a18421b3805ff8c84ada6b030439c1e8feb8c0c
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-11-01 13:19:09 +0700

    Added example for loading icon

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import SettingsPage from "@/views/SettingsPage.vue";
 import StorePage from "@/views/StorePage.vue";
 import ArticlePage from "@/views/ArticlePage.vue";
 import ChallengesPage from "@/views/ChallengesPage.vue";
+import Store from "@/store";
 
 Vue.use(VueRouter);
 
@@ -28,6 +29,7 @@ const routes = [
 
 const router = new VueRouter({
   routes,
+  Store,
 });
 
 // Below function is for moving back to the top of the page on route change
@@ -40,5 +42,20 @@ router.beforeEach((to, from, next) => {
   }
   next();
 });
+
+// BELOW: DUMMY FOR LOADING STATE EXAMPLE
+
+// router.beforeEach((to, from, next) => {
+//   Store.state.isLoading = true; // Set isLoading to true initially
+//   setTimeout(() => {
+//     window.scrollTo(0, 0);
+//     const contentContainer = document.querySelector(".content-container");
+//     if (contentContainer) {
+//       contentContainer.scrollTop = 0; // Scrolls to the top of the content container
+//     }
+//     Store.state.isLoading = false; // Set isLoading to false after 2 seconds
+//     next();
+//   }, 2000);
+// });
 
 export default router;
