31068a2f750f082d91a5b4f1b05ed56f60d145eb
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-11-07 14:35:49 +0700

    Button background color

--- src/components/NewsFeed/NewsFeed.vue
+++ src/components/NewsFeed/NewsFeed.vue
@@ -672,20 +672,20 @@ export default {
 }
 
 .post-button {
-  background-color: #4caf50;
+  background-color: #3a59c0;
   color: white;
   padding: 10px 20px;
   border: none;
   border-radius: 4px;
   cursor: pointer;
   font-size: 16px;
-  transition: background-color 0.3s;
+  /* transition: background-color 0.3s; */
   margin-bottom: 10px;
 }
 
-.post-button:hover {
-  background-color: #45a049;
-}
+/* .post-button:hover {
+  background-color: #3a59c0;
+} */
 
 .status-form {
   margin-right: 10px;
