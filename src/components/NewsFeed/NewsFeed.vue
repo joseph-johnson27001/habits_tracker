37e3c6834cb727e6c48712d30f2cbca5f2a63914
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-11-10 13:49:28 +0700

    Status input focus box shadow

--- src/components/NewsFeed/NewsFeed.vue
+++ src/components/NewsFeed/NewsFeed.vue
@@ -675,7 +675,7 @@ export default {
 
 .status-input:focus {
   border: 1px solid #6da9e4;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   outline: none;
 }
 
