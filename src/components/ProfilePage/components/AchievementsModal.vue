2da7f796559012f21356fc0dddd01aa01431b624
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-11-15 13:00:51 +0700

    Achievements Modal

---
+++ src/components/ProfilePage/components/AchievementsModal.vue
@@ -0,0 +1,36 @@
+<!-- AchievementModal.vue -->
+<template>
+  <div class="achievement-modal-overlay" v-if="isVisible" @click="closeModal">
+    <div class="achievement-modal">
+      <div class="modal-content">
+        <h2 class="achievement-title">{{ selectedAchievement.title }}</h2>
+        <span class="achievement-icon">{{ selectedAchievement.icon }}</span>
+        <p class="achievement-description">
+          {{ selectedAchievement.description }}
+        </p>
+        <p class="achieved-date">
+          Achieved: {{ selectedAchievement.achieved }}
+        </p>
+        <button class="close-button" @click="closeModal">Close</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isVisible: Boolean,
+    selectedAchievement: Object,
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your modal styles here */
+</style>
