acce41b6ae3907fb5d373073cd892b3eb97fafc6
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-11-16 12:11:16 +0700

    Edit profile layout

--- src/views/EditProfilePage.vue
+++ src/views/EditProfilePage.vue
@@ -1,5 +1,103 @@
 <template>
   <div class="card">
-    <h1>EDIT PROFILE PAGE</h1>
+    <h1>EDIT PROFILE</h1>
+
+    <!-- Edit First and Last Name -->
+    <div class="form-group">
+      <label for="fullName">Name</label>
+      <input type="text" id="fullName" v-model="fullName" />
+    </div>
+
+    <!-- Edit User Title -->
+    <div class="form-group">
+      <label for="userTitle">User Title</label>
+      <input type="text" id="userTitle" v-model="userTitle" />
+    </div>
+
+    <!-- Edit Badges Display Preference -->
+    <div class="form-group">
+      <label for="displayBadges">Display Badges</label>
+      <select id="displayBadges" v-model="displayBadges" multiple>
+        <!-- Options for achievements go here -->
+      </select>
+    </div>
+
+    <!-- Edit Latest Achievements -->
+    <div class="form-group">
+      <label for="latestAchievements">Latest Achievements</label>
+      <select id="latestAchievements" v-model="selectedAchievements" multiple>
+        <!-- Options for achievements go here -->
+      </select>
+    </div>
+
+    <!-- Edit Main Habit -->
+    <div class="form-group">
+      <label for="mainHabit">Main Habit</label>
+      <select id="mainHabit" v-model="selectedMainHabit">
+        <!-- Options for habits go here -->
+      </select>
+    </div>
+
+    <!-- Save Button -->
+    <button @click="saveChanges">Save Changes</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      fullName: "",
+      userTitle: "", // User's title
+      displayBadges: false, // Whether to display badges or not
+      selectedAchievements: [], // Array to hold selected achievements
+      selectedMainHabit: "", // Hold the selected main habit
+    };
+  },
+  methods: {
+    saveChanges() {
+      // Implement logic to save changes
+      // You can access the updated data from this.firstName, this.lastName, etc.
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-group {
+  margin-bottom: 20px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input,
+select {
+  width: 100%;
+  padding: 10px 0px 10px 10px;
+  outline: none !important;
+}
+
+input:focus,
+select:focus {
+  border: 1px solid #6da9e4;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+button {
+  width: 100%;
+  padding: 10px;
+  background-color: #3498db;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+}
+
+button:hover {
+  background-color: #297fb8;
+}
+</style>
