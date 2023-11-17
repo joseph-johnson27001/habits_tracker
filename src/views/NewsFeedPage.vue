61b10fa85081e32b381ca93ba041758351ee2116
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-11-17 11:47:17 +0700

    Reverted changes which broke everything

--- src/views/NewsFeedPage.vue
+++ src/views/NewsFeedPage.vue
@@ -1,18 +1,9 @@
 <template>
   <div class="news" v-show="!this.$store.state.isLoading">
-    <div class="switch-container">
-      <label class="switch">
-        <input type="checkbox" v-model="showSocialStats" />
-        <span class="slider"></span>
-      </label>
-      <span>{{
-        showSocialStats ? "Show Social Stats" : "Show News Feed"
-      }}</span>
-    </div>
-    <div class="news-feed" v-show="!this.showSocialStats">
+    <div class="news-feed">
       <NewsFeed />
     </div>
-    <div class="social-stats" v-shpw="this.socialStats">
+    <div class="social-stats" v-show="showSocialStats">
       <SocialStats />
     </div>
   </div>
@@ -30,8 +21,7 @@ export default {
   },
   data() {
     return {
-      showSwitch: true,
-      showSocialStats: false,
+      showSocialStats: true,
     };
   },
   mounted() {
@@ -42,12 +32,8 @@ export default {
     window.removeEventListener("resize", this.handleViewportChange);
   },
   methods: {
-    // handleViewportChange() {
-    //   this.showSocialStats = window.innerWidth > 992;
-    // },
-    toggleSocialStats() {
-      this.showSocialStats.style.display = "block";
-      this.NewsFeed.style.display = "none";
+    handleViewportChange() {
+      this.showSocialStats = window.innerWidth > 992;
     },
   },
 };
@@ -63,72 +49,13 @@ export default {
   padding-left: 5px;
 }
 
-.switch-container {
-  display: flex;
-  align-items: center;
-  margin-bottom: 10px;
-}
-
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 40px;
-  height: 20px;
-}
-
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  transition: 0.4s;
-  border-radius: 20px;
-}
-
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 16px;
-  width: 16px;
-  left: 2px;
-  bottom: 2px;
-  background-color: white;
-  transition: 0.4s;
-  border-radius: 50%;
-}
-
-input:checked + .slider {
-  background-color: #2196f3;
-}
-
-input:checked + .slider:before {
-  transform: translateX(20px);
-}
-
-@media (min-width: 993px) {
-  .news-feed {
-    display: block;
-  }
-  .switch-container {
-    display: none;
-  }
-}
-
 @media (max-width: 992px) {
   .news {
     grid-template-columns: 1fr;
   }
-  .switch-container {
-    display: block;
+
+  .social-stats {
+    display: none;
   }
 }
 </style>
