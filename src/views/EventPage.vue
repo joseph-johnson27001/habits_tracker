a4c54a3c85cfdfb71582b7079329fbfb49241dbe
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-12-05 11:58:03 +0700

    More details

--- src/views/EventPage.vue
+++ src/views/EventPage.vue
@@ -3,12 +3,28 @@
     <h1>{{ event.title }}</h1>
     <p>{{ event.description }}</p>
     <p><strong>Date:</strong> {{ event.date }}</p>
+
     <h2>Awards</h2>
     <ul>
       <li v-for="award in event.awards" :key="award.name">
         <span class="award-icon">{{ award.icon }}</span> {{ award.name }}
       </li>
     </ul>
+
+    <h2>Details</h2>
+    <p><strong>Location:</strong> {{ event.location }}</p>
+    <p><strong>Time:</strong> {{ event.time }}</p>
+    <p><strong>Organizer:</strong> {{ event.organizer }}</p>
+
+    <h2>Activities</h2>
+    <ul>
+      <li v-for="activity in event.activities" :key="activity">
+        {{ activity }}
+      </li>
+    </ul>
+
+    <h2>Special Instructions</h2>
+    <p>{{ event.specialInstructions }}</p>
   </div>
 </template>
 
@@ -25,6 +41,17 @@ export default {
           { name: "Fitness Fanatic Badge", icon: "🏋️" },
           { name: "Health Enthusiast Title", icon: "💪" },
         ],
+        location: "City Park",
+        time: "10:00 AM - 5:00 PM",
+        organizer: "Fitness Galaxy",
+        activities: [
+          "Group workout sessions",
+          "Healthy cooking workshops",
+          "Fitness challenges",
+          "Yoga in the park",
+        ],
+        specialInstructions:
+          "Please bring your own yoga mat. Wear comfortable workout attire.",
         // Add more details as needed
       },
     };
