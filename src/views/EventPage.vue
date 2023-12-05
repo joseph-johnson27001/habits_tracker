35f82f9d2f1a03f2233dc32d8fe398d8970542ff
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-12-05 11:55:52 +0700

    Starter event dummy code

--- src/views/EventPage.vue
+++ src/views/EventPage.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="card">
-    <h1>EVENT DETAILS HERE</h1>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.description }}</p>
+    <p><strong>Date:</strong> {{ event.date }}</p>
+    <h2>Awards</h2>
+    <ul>
+      <li v-for="award in event.awards" :key="award.name">
+        <span class="award-icon">{{ award.icon }}</span> {{ award.name }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  name: "EventPage",
+  data() {
+    return {
+      event: {
+        title: "Fitness Expo",
+        description: "Join our fitness expo for a day of fun and exercise!",
+        date: "2023-11-15",
+        awards: [
+          { name: "Fitness Fanatic Badge", icon: "🏋️" },
+          { name: "Health Enthusiast Title", icon: "💪" },
+        ],
+        // Add more details as needed
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.award-icon {
+  margin-right: 5px;
+}
+</style>
