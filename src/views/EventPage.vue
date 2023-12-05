d32291ec638f48a84ce187dc9875c9a4148c0ab9
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2023-12-05 12:04:42 +0700

    Media query layouts

--- src/views/EventPage.vue
+++ src/views/EventPage.vue
@@ -1,30 +1,42 @@
 <template>
   <div class="card">
     <h1>{{ event.title }}</h1>
-    <p>{{ event.description }}</p>
-    <p><strong>Date:</strong> {{ event.date }}</p>
+    <div class="event-grid">
+      <div class="description-section">
+        <p class="description">{{ event.description }}</p>
+        <p><strong>Date:</strong> {{ event.date }}</p>
+      </div>
 
-    <h2>Awards</h2>
-    <ul>
-      <li v-for="award in event.awards" :key="award.name">
-        <span class="award-icon">{{ award.icon }}</span> {{ award.name }}
-      </li>
-    </ul>
+      <div class="awards-section">
+        <h2>Awards</h2>
+        <ul>
+          <li v-for="award in event.awards" :key="award.name">
+            <span class="award-icon">{{ award.icon }}</span> {{ award.name }}
+          </li>
+        </ul>
+      </div>
 
-    <h2>Details</h2>
-    <p><strong>Location:</strong> {{ event.location }}</p>
-    <p><strong>Time:</strong> {{ event.time }}</p>
-    <p><strong>Organizer:</strong> {{ event.organizer }}</p>
+      <div class="details-section">
+        <h2>Details</h2>
+        <p><strong>Location:</strong> {{ event.location }}</p>
+        <p><strong>Time:</strong> {{ event.time }}</p>
+        <p><strong>Organizer:</strong> {{ event.organizer }}</p>
+      </div>
 
-    <h2>Activities</h2>
-    <ul>
-      <li v-for="activity in event.activities" :key="activity">
-        {{ activity }}
-      </li>
-    </ul>
+      <div class="activities-section">
+        <h2>Activities</h2>
+        <ul>
+          <li v-for="activity in event.activities" :key="activity">
+            {{ activity }}
+          </li>
+        </ul>
+      </div>
 
-    <h2>Special Instructions</h2>
-    <p>{{ event.specialInstructions }}</p>
+      <div class="instructions-section">
+        <h2>Special Instructions</h2>
+        <p>{{ event.specialInstructions }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -60,7 +72,58 @@ export default {
 </script>
 
 <style scoped>
+.event-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+}
+
+.description-section {
+  grid-column: span 2;
+}
+
+.description {
+  font-size: 18px;
+  color: #333;
+}
+
+.awards-section,
+.details-section,
+.activities-section,
+.instructions-section {
+  margin-top: 20px;
+}
+
+.awards-section h2,
+.details-section h2,
+.activities-section h2,
+.instructions-section h2 {
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+
+.awards-section ul,
+.details-section ul,
+.activities-section ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.awards-section li,
+.details-section li,
+.activities-section li {
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+
 .award-icon {
   margin-right: 5px;
+}
+
+@media (max-width: 992px) {
+  .event-grid {
+    display: block;
+  }
 }
 </style>
